f89575bdc58bd9e2a32bfb161de64186e04d63b2
Wouter <tuvokki@gmail.com>
2015-01-11 23:10:52 +0100

    Done refactoring this

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -28,13 +28,13 @@ app.directive('sharedHeader', function() {
 
 app.directive("ipList", function() {
   return {
-    template: '<div class="wrap head"> <div class="col1of4"> <span>Ip:<br></span> </div> <div class="col1of4"> <span>Comment:<br></span> </div> <div class="col1of4"> <span>Host:<br></span> </div> <div class="col1of4"> <span>Date:<br></span> </div> </div> <div class="wrap"> <hr class="col4of4"> </div> <div class="wrap" ng-repeat="iprecord in iplist"> <div class="col1of4"> {{ iprecord.ip }} </div> <div class="col1of4"> {{ iprecord.msg }} </div> <div class="col1of4"> {{ iprecord.host }} </div> <div class="col1of4"> {{ iprecord.date }} </div> </div>'
+    template: '<div class="wrap head"> <div class="col1of4"> <span>Ip:</span> </div> <div class="col1of4"> <span>Comment:</span> </div> <div class="col1of4"> <span>Host:</span> </div> <div class="col1of4"> <span>Date:</span> </div> </div> <div class="wrap data"> <div ng-repeat="iprecord in iplist" class="record"> <div class="col1of4"> {{ iprecord.ip }} </div> <div class="col1of4"> {{ iprecord.msg }} </div> <div class="col1of4"> {{ iprecord.host }} </div> <div class="col1of4"> {{ iprecord.date | date : "MM/dd/yyyy h:mma" }} </div> </div> </div>'
   };
 });
 
 app.directive("ipForm", function() {
   return {
-    template: '<div class="wrap"> <hr> <form id="ipsave" ng-submit="submitIp()"> <div class="col1of4"> <span>Your ip:<br></span> <span class="id">{{ myip }}</span> </div> <div class="col1of4"> <label for="comment">comment:<br> <input ng-model="message" id="comment" type="text" value="" placeholder=" - Type your comment - " name="comment"> </label> <input id="host" type="hidden" value="frontend" name="host"> </div> <div class="col1of4"> <span>Host:<br></span> frontend </div> <div class="col1of4"> <span>Save now:<br></span> <input type="submit" value="save"> </div> </form> </div>'
+    template: '<div class="wrap form"> <form id="ipsave" ng-submit="submitIp()"> <div class="col1of4"> <h6>Your ip:</h6> <span>{{ myip }}</span> </div> <div class="col1of4"> <h6>Comment:</h6> <input ng-model="message" id="comment" type="text" value="" placeholder=" - Type your comment - " name="comment"> </div> <div class="col1of4"> <h6>Host:</h6> frontend <input id="host" type="hidden" value="frontend" name="host"> </div> <div class="col1of4"> <h6>Save now:</h6> <input type="submit" value="save"> </div> </form> </div>'
   };
 });
 
--- src/javascript/directives/ipListDirective.coffee
+++ src/javascript/directives/ipListDirective.coffee
@@ -2,57 +2,54 @@ app.directive "ipList", ->
   template: '
 <div class="wrap head">
   <div class="col1of4">
-    <span>Ip:<br></span>
+    <span>Ip:</span>
   </div>
   <div class="col1of4">
-    <span>Comment:<br></span>
+    <span>Comment:</span>
   </div>
   <div class="col1of4">
-    <span>Host:<br></span>
+    <span>Host:</span>
   </div>
   <div class="col1of4">
-    <span>Date:<br></span>
+    <span>Date:</span>
   </div>
 </div>
-<div class="wrap">
-  <hr class="col4of4">
-</div>  
-<div class="wrap" ng-repeat="iprecord in iplist">
-  <div class="col1of4">
-    {{ iprecord.ip }}
-  </div>
-  <div class="col1of4">
-    {{ iprecord.msg }}
-  </div>
-  <div class="col1of4">
-    {{ iprecord.host }}
-  </div>
-  <div class="col1of4">
-    {{ iprecord.date }}
+<div class="wrap data">
+  <div ng-repeat="iprecord in iplist" class="record">
+    <div class="col1of4">
+      {{ iprecord.ip }}
+    </div>
+    <div class="col1of4">
+      {{ iprecord.msg }}
+    </div>
+    <div class="col1of4">
+      {{ iprecord.host }}
+    </div>
+    <div class="col1of4">
+      {{ iprecord.date | date : "MM/dd/yyyy h:mma" }}
+    </div>
   </div>
 </div>'
 
 app.directive "ipForm", ->
   template: '
-<div class="wrap">
-  <hr>
+<div class="wrap form">
   <form id="ipsave" ng-submit="submitIp()">
     <div class="col1of4">
-      <span>Your ip:<br></span>
-      <span class="id">{{ myip }}</span>
+      <h6>Your ip:</h6>
+      <span>{{ myip }}</span>
     </div>
     <div class="col1of4">
-        <label for="comment">comment:<br>
-          <input ng-model="message" id="comment" type="text" value="" placeholder=" - Type your comment - " name="comment">
-        </label>
-        <input id="host" type="hidden" value="frontend" name="host">
+      <h6>Comment:</h6>
+      <input ng-model="message" id="comment" type="text" value="" placeholder=" - Type your comment - " name="comment">
     </div>
     <div class="col1of4">
-      <span>Host:<br></span>
+      <h6>Host:</h6>
       frontend
+      <input id="host" type="hidden" value="frontend" name="host">
     </div>
     <div class="col1of4">
-      <span>Save now:<br></span>
+      <h6>Save now:</h6>
       <input type="submit" value="save">
     </div>
   </form>
